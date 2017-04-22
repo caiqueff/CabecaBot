fdd8f42accf67f51749c78ce4a6aa734a01426a8
Caique F <caiquecff@live.ca>
2017-04-22 15:57:29 -0300

    Novo link Ed

--- bot.php
+++ bot.php
@@ -6,22 +6,14 @@ require "funcoes.php";
 $dados = json_decode(file_get_contents('php://input'), true);
 $link = "https://api.telegram.org/bot".$token;
 
-/////////////////////////////////////////////////////////////////
-$fp = fopen('telegram.txt', 'a');
-fwrite($fp, print_r($dados, true));
-fclose($fp);
-/////////////////////////////////////////////////////////////////
-
 $msg = $dados['message']['text'];
 
 if((stristr($msg, $nome) || $dados['message']['chat']['type'] == "private") && !isCommand($msg)){
 
-    /**
-    Entra no IF se:
-    -(O nome for citado num grupo ou a mensagem vier do chat privado) e (a mensagem não for um comando)
-    **/
+    //    Entra no IF se:
+    //    (O nome for citado num grupo ou a mensagem vier do chat privado) e (a mensagem não for um comando)
 
-    $resp = file_get_contents('http://ed.conpet.gov.br:8085/?msg='.rawurlencode(str_replace($nome,'Ed',strtolower($msg))));
+    $resp = file_get_contents('http://www.ed.conpet.gov.br/cgi-bin/bot_gateway.cgi?server=bot.insite.com.br:8085&pure=1&msg='.rawurlencode(str_replace($nome,'Ed',strtolower($msg))));
 
     $params = array(
             'chat_id' => $dados['message']['chat']['id'],
